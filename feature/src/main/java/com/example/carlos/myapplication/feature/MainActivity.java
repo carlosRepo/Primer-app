2a5d609f5e5e836a91967f11e27ea3549ec62a1b
carlospoqw <carlospoqw@gmail.com>
2019-01-12 20:03:26 -0300

    Segundo commit Logueo Validado sin base de datos

--- feature/src/main/java/com/example/carlos/myapplication/feature/MainActivity.java
+++ feature/src/main/java/com/example/carlos/myapplication/feature/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.carlos.myapplication.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
+        //Obtener todos los elementos de la vista
+        Button botonLoguear=(Button) findViewById(R.id.buttonEnviar);
+        final EditText textNick=(EditText) findViewById(R.id.editTextCorreo);
+        final EditText textPassword=(EditText) findViewById(R.id.editTextPassword);
+
+        //Evento boton Loguear
+        botonLoguear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textNick.getText().toString().trim().isEmpty()){
+                    Toast.makeText(MainActivity.this,("Ingrese un correo."),Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (textPassword.getText().toString().trim().isEmpty()){
+                    Toast.makeText(MainActivity.this,("Ingrese su contraseña."),Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Toast.makeText(MainActivity.this,("Logueado Exitosamente."),Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+
 }
